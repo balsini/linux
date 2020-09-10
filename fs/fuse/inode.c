db4c68d5f019fc4dd7332460d3164d69fd233dcb
Alessio Balsini <balsini@android.com>
2020-09-10 17:57:36 +0100

    fuse: Definitions and ioctl() for passthrough
    Introduce the new FUSE passthrough ioctl(), which allows userspace to
    specify a direct connection between a FUSE file and a lower file system
    file.
    Such ioctl() requires userspace to specify:
    - the file descriptor of one of its opened files,
    - the unique identifier of the FUSE request associated with a pending
      open/create operation,
    both encapsulated into a fuse_passthrough_out data structure.
    The ioctl() will search for the pending FUSE request matching the unique
    identifier, and update the passthrough file pointer of the request with the
    file pointer referenced by the passed file descriptor.
    When that pending FUSE request is handled, the passthrough file pointer
    is copied to the fuse_file data structure, so that the link between FUSE
    and lower file system is consolidated.
    
    In order for the passthrough mode to be successfully activated, the lower
    file system file must implement both read_ and write_iter file operations.
    This extra check avoids special pseudofiles to be targets for this feature.
    An additional enforced limitation is that when FUSE passthrough is enabled,
    no further file system stacking is allowed.
    
    Signed-off-by: Alessio Balsini <balsini@android.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -993,6 +993,12 @@ static void process_init_reply(struct fuse_conn *fc, struct fuse_args *args,
 			    !fuse_dax_check_alignment(fc, arg->map_alignment)) {
 				ok = false;
 			}
+			if (arg->flags & FUSE_PASSTHROUGH) {
+				fc->passthrough = 1;
+				/* Prevent further stacking */
+				fc->sb->s_stack_depth =
+					FILESYSTEM_MAX_STACK_DEPTH;
+			}
 		} else {
 			ra_pages = fc->max_read / PAGE_SIZE;
 			fc->no_lock = 1;
@@ -1035,7 +1041,8 @@ void fuse_send_init(struct fuse_conn *fc)
 		FUSE_WRITEBACK_CACHE | FUSE_NO_OPEN_SUPPORT |
 		FUSE_PARALLEL_DIROPS | FUSE_HANDLE_KILLPRIV | FUSE_POSIX_ACL |
 		FUSE_ABORT_ERROR | FUSE_MAX_PAGES | FUSE_CACHE_SYMLINKS |
-		FUSE_NO_OPENDIR_SUPPORT | FUSE_EXPLICIT_INVAL_DATA;
+		FUSE_NO_OPENDIR_SUPPORT | FUSE_EXPLICIT_INVAL_DATA |
+		FUSE_PASSTHROUGH;
 #ifdef CONFIG_FUSE_DAX
 	if (fc->dax)
 		ia->in.flags |= FUSE_MAP_ALIGNMENT;
