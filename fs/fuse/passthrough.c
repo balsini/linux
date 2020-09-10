61dbc82003f7919ba1c2727b4478905d27fc05cd
Alessio Balsini <balsini@android.com>
2020-09-10 18:36:29 +0100

    fuse: Introduce synchronous read and write for passthrough
    All the read and write operations performed on fuse_files which have the
    passthrough feature enabled are forwarded to the associated lower file
    system file.
    
    Sending the request directly to the lower file system avoids the userspace
    round-trip that, because of possible context switches and additional
    operations might reduce the overall performance, especially in those cases
    where caching doesn't help, for example in reads at random offsets.
    
    The connection between the fuse_files and lower file system file is created
    through the FUSE_DEV_IOC_PASSTHROUGH_OPEN ioctl() and can be verified if
    the passthrough_filp pointer in the fuse_file is not null.
    After the read/write operation is completed, both the access/modify
    operations must be notified (and propagated) to the lower file system,
    while this is not necessary for the FUSE file system because automatically
    triggered by the read/write caller.
    This change only implements synchronous requests in passthrough, returning
    an error in the case of ansynchronous operations, yet covering the majority
    of the use cases.
    
    Signed-off-by: Alessio Balsini <balsini@android.com>

--- fs/fuse/passthrough.c
+++ fs/fuse/passthrough.c
@@ -2,6 +2,87 @@
 
 #include "fuse_i.h"
 
+#include <linux/fs_stack.h>
+#include <linux/fsnotify.h>
+#include <linux/uio.h>
+
+static void fuse_copyattr(struct file *dst_file, struct file *src_file,
+			  bool write)
+{
+	if (write) {
+		struct inode *dst = file_inode(dst_file);
+		struct inode *src = file_inode(src_file);
+
+		fsnotify_modify(src_file);
+		fsstack_copy_inode_size(dst, src);
+	} else {
+		fsnotify_access(src_file);
+	}
+}
+
+
+ssize_t fuse_passthrough_read_iter(struct kiocb *iocb_fuse,
+				   struct iov_iter *iter)
+{
+	ssize_t ret;
+	struct file *fuse_filp = iocb_fuse->ki_filp;
+	struct fuse_file *ff = fuse_filp->private_data;
+	struct file *passthrough_filp = ff->passthrough_filp;
+
+	if (!iov_iter_count(iter))
+		return 0;
+
+	if (is_sync_kiocb(iocb_fuse)) {
+		struct kiocb iocb;
+
+		kiocb_clone(&iocb, iocb_fuse, passthrough_filp);
+		ret = call_read_iter(passthrough_filp, &iocb, iter);
+		iocb_fuse->ki_pos = iocb.ki_pos;
+		if (ret >= 0)
+			fuse_copyattr(fuse_filp, passthrough_filp, false);
+
+	} else {
+		ret = -EIO;
+	}
+
+	return ret;
+}
+
+ssize_t fuse_passthrough_write_iter(struct kiocb *iocb_fuse,
+				    struct iov_iter *iter)
+{
+	ssize_t ret;
+	struct file *fuse_filp = iocb_fuse->ki_filp;
+	struct fuse_file *ff = fuse_filp->private_data;
+	struct inode *fuse_inode = file_inode(fuse_filp);
+	struct file *passthrough_filp = ff->passthrough_filp;
+
+	if (!iov_iter_count(iter))
+		return 0;
+
+	inode_lock(fuse_inode);
+
+	if (is_sync_kiocb(iocb_fuse)) {
+		struct kiocb iocb;
+
+		kiocb_clone(&iocb, iocb_fuse, passthrough_filp);
+
+		file_start_write(passthrough_filp);
+		ret = call_write_iter(passthrough_filp, &iocb, iter);
+		file_end_write(passthrough_filp);
+
+		iocb_fuse->ki_pos = iocb.ki_pos;
+		if (ret > 0)
+			fuse_copyattr(fuse_filp, passthrough_filp, true);
+	} else {
+		ret = -EIO;
+	}
+
+	inode_unlock(fuse_inode);
+
+	return ret;
+}
+
 int fuse_passthrough_setup(struct fuse_req *req, unsigned int fd)
 {
 	int ret;
